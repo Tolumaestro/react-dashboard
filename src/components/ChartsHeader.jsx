02fe855b829fd7ba34615d3c10ea76f00b09d574
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-08-18 13:18:09 +0100

    first commit

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsHEader = () => {
+  return <div>ChartsHEader</div>;
+};
+
+export default ChartsHEader;
---
+++ src/pages/Charts/ColorMapping.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ColorMApping = () => {
+  return <div>ColorMApping</div>;
+};
+
+export default ColorMApping;
